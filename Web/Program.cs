8225cb9ab7c5e3ca208ccb33a8a082b1aaf009e9
Ignacio Carignano <nachocarignano@gmail.com>
2025-10-14 00:52:29 -0300

    listo el inicio de sesion y la pantalla principal

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using Web.Auth;
+using Web.Services;
 
 namespace Web
 {
@@ -10,8 +14,16 @@ namespace Web
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
+            var url = "https://localhost:7123";
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(url) });
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddMudServices();
+            builder.Services.AddScoped<AuthenticationProviderJWT>();
+            builder.Services.AddScoped<AuthService>();
+            builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderJWT>(x => x.GetRequiredService<AuthenticationProviderJWT>());
+            builder.Services.AddScoped<ILoginService, AuthenticationProviderJWT>(x => x.GetRequiredService<AuthenticationProviderJWT>());
+
 
             await builder.Build().RunAsync();
         }
